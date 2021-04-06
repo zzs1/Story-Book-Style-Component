1e21aac5a9649f34eaa6844b9a3c07f38110897e
joycechan <ychan119@my.bcit.ca>
2021-04-06 13:44:54 -0700

    topbar

---
+++ myapp/comps/TopBar/index.js
@@ -0,0 +1,12 @@
+function TopBar() {
+    return (
+      < div className={styles.TopBar}>
+        <img src="https:placekitten.com/50/50" 
+        style={{
+          width:100,
+          height:30,
+        }}/>
+      </div>
+    )
+  }
+  export default TopBar;
