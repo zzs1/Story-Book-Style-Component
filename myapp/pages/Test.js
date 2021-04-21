9a69bc8f25b50c3ffd3201661ef752c1f42e752a
zzs1 <35611324+zzs1@users.noreply.github.com>
2021-04-20 22:21:58 -0700

    Delete myapp directory

--- myapp/pages/Test.js
+++
@@ -1,7 +0,0 @@
-import styles from '../styles/Home.module.css'
-import Steps from '../comps/Steps'
-import Result from '../comps/Result'
-import Overlay from '../comps/Overlay'
-import picturebutton from '../comps/picturebutton'
-
-
