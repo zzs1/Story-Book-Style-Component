80d2d0be97f02968a6c63865f6a5677b87325102
ChisakiNakamura <cnakamura1@my.bcit.ca>
2021-05-18 18:05:45 -0700

    hovering colors for button added

---
+++ .history/Chisaki/myapp/comps/Button/index2_20210518172147.js
@@ -0,0 +1,57 @@
+import styles from '../../styles/Home.module.css'
+import React from 'react'
+import styled from 'styled-components';
+import {useRouter} from 'next/router' 
+
+const CardCont = styled.div`
+background:${props=>props.bgcolor};
+min-width:100px;
+height:50px;
+border-radius:10px;
+display:flex;
+flex-direction:column;
+text-align:center;
+color:#FFFFFF;
+font-family:Roboto, sans-serif;
+margin:30px;
+justify-content:center;
+align-items:center;
+`;
+  
+const Heading = styled.h5`
+margin:0px;
+justify-content:center;
+margin-bottom:5px;
+margin-top:5px;
+width:90%;
+`;
+
+const Ps = styled.p`
+margin:0px;
+justify-content:center;
+margin-bottom:5px;
+margin-top:5px;
+font-size:12px;
+`;
+
+const ButtonUI = ({
+title="NEXT",
+subtext="Question2",
+routeTo='/question1',
+bgcolor="#82DED9",
+
+}) => {
+  const router = useRouter();
+  return <CardCont 
+  onClick={()=>router.push(routeTo)} 
+  bgcolor={bgcolor}
+  className={styles.hoverbtn2}
+  >
+    <Heading
+     className={styles.hoverbtn2}>{title}</Heading>
+<Ps>{subtext}</Ps>
+  </CardCont>
+}
+
+
+export default ButtonUI;
